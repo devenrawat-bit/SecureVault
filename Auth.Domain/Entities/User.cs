bc88392a23eca67ea75c77239e7882c25d8a611a
Deven-Evince <deven@evincedev.com>
2026-09-21 10:51:11 +0530

    first_microservice

---
+++ Auth.Domain/Entities/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Auth.Domain.Entities
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+
+        public string Email { get; set; } = string.Empty;
+
+        public string FirstName { get; set; } = string.Empty;
+
+        public string LastName { get; set; } = string.Empty;
+
+        public Guid OrganizationId { get; set; }
+
+        public bool IsActive { get; set; } = true;
+
+        public DateTime CreatedOn { get; set; }
+
+        public DateTime? ModifiedOn { get; set; }
+    }
+}
